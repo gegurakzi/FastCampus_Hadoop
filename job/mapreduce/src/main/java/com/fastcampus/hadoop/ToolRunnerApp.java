6c3a1cb6459a4e7c41c7c265e418090ccc081523
Malachai <xogmldntmd@naver.com>
2022-11-30 21:53:46 +0900

    Hadoop MapReduce Job development +added: simple ToolRunner application +updated: simple GenericOptionsParser application

--- job/mapreduce/src/main/java/com/fastcampus/hadoop/ToolRunnerApp.java
+++ job/mapreduce/src/main/java/com/fastcampus/hadoop/ToolRunnerApp.java
@@ -2,29 +2,31 @@ package com.fastcampus.hadoop;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
-import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.util.GenericOptionsParser;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
 import java.util.Arrays;
 
-public class GenericOptionsParserApp {
 
+public class ToolRunnerApp extends Configured implements Tool {
 
-    public static void main(String[] args) throws Exception {
-        // 1. 모든 argument 확인
-        System.out.println("1. "+Arrays.toString(args));
-
-        Configuration conf = new Configuration();
-        GenericOptionsParser optionsParser = new GenericOptionsParser(conf, args);
+    @Override
+    public int run(String[] strings) throws Exception {
+        Configuration conf = getConf();
         String val1 = conf.get("mapreduce.map.memory.mb");
         Boolean val2 = conf.getBoolean("job.test", false);
         // 2. 옵션으로 파싱 된 인자 확인
         System.out.println("2. "+"mapreduce.map.memory.mb : "+val1+", "+"job.test : "+val2);
 
-        String[] remainingArgs = optionsParser.getRemainingArgs();
         // 3. 옵션 파싱 후 나머지 argument 확인
-        System.out.println("3. "+Arrays.toString(remainingArgs));
+        System.out.println("3. "+Arrays.toString(strings));
+        return 0;
+    }
+
+    public static void main(String[] args) throws Exception {
+        // 1. 모든 argument 확인
+        System.out.println("1. "+Arrays.toString(args));
+        int exitCode = ToolRunner.run(new ToolRunnerApp(), args);
+        System.out.println(exitCode);
     }
 }
