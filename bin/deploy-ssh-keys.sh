739e7c0f012541b621e4565c16d7fc78eb28d02c
Malachai <xogmldntmd@naver.com>
2022-12-27 21:52:38 +0900

    Hadoop-Spark Cluster development +updated: deploying ssh authorized_keys

--- bin/deploy-ssh-keys.sh
+++ bin/deploy-ssh-keys.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-sudo docker exec -u 0 master01 /bin/bash && /usr/sbin/sshd-keygen -A && /usr/sbin/sshd && ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa
+sudo docker exec --user root master01 /bin/bash && /usr/sbin/sshd-keygen -A && /usr/sbin/sshd && ssh-keygen -t rsa -P '' -f /root/.ssh/id_rsa
 
-sudo docker exec -u 0 master02 /bin/bash && /usr/sbin/sshd-keygen -A && /usr/sbin/sshd && ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa
+sudo docker exec --user root master02 /bin/bash && /usr/sbin/sshd-keygen -A && /usr/sbin/sshd && ssh-keygen -t rsa -P '' -f /root/.ssh/id_rsa
 
-sudo docker exec -u 0 slave01 /bin/bash && /usr/sbin/sshd-keygen -A && /usr/sbin/sshd && ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa
+sudo docker exec --user root slave01 /bin/bash && /usr/sbin/sshd-keygen -A && /usr/sbin/sshd && ssh-keygen -t rsa -P '' -f /root/.ssh/id_rsa
 
-sudo docker exec -u 0 slave02 /bin/bash && /usr/sbin/sshd-keygen -A && /usr/sbin/sshd && ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa
+sudo docker exec --user root slave02 /bin/bash && /usr/sbin/sshd-keygen -A && /usr/sbin/sshd && ssh-keygen -t rsa -P '' -f /root/.ssh/id_rsa
 
-sudo docker exec -u 0 slave03 /bin/bash && /usr/sbin/sshd-keygen -A && /usr/sbin/sshd && ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa
+sudo docker exec --user root slave03 /bin/bash && /usr/sbin/sshd-keygen -A && /usr/sbin/sshd && ssh-keygen -t rsa -P '' -f /root/.ssh/id_rsa
