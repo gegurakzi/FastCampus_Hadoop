a0fca870c3e1b12b03851601ba35499319e55910
Malachai <xogmldntmd@naver.com>
2022-12-27 21:49:51 +0900

    Hadoop-Spark Cluster development +updated: deploying ssh authorized_keys

--- bin/deploy-ssh-authorized-keys.sh
+++ bin/deploy-ssh-authorized-keys.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 get_authorized_keys() {
-  local m1=sudo docker exec master01 cat ~/.ssh/id_rsa.pub
+  local m1=$(sudo docker exec master01 cat ~/.ssh/id_rsa.pub)
 
-  local m2=sudo docker exec master02 cat ~/.ssh/id_rsa.pub
+  local m2=$(sudo docker exec master02 cat ~/.ssh/id_rsa.pub)
 
-  local s1=sudo docker exec slave01 cat ~/.ssh/id_rsa.pub
+  local s1=$(sudo docker exec slave01 cat ~/.ssh/id_rsa.pub)
 
-  local s2=sudo docker exec slave02 cat ~/.ssh/id_rsa.pub
+  local s2=$(sudo docker exec slave02 cat ~/.ssh/id_rsa.pub)
 
-  local s3=sudo docker exec slave03 cat ~/.ssh/id_rsa.pub
+  local s3=$(sudo docker exec slave03 cat ~/.ssh/id_rsa.pub)
 
   echo $m1
   echo $m2
